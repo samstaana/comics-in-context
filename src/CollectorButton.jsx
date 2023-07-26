0111ac6f501328402c1029d61b2d79262717edf6
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-07-25 22:37:24 -0700

    finished

---
+++ src/CollectorButton.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+
+export default function CollectorButton() {
+    const buttonStyling = React.useContext(StylesContext)
+  
+    return (
+        <div>
+        <button>
+            Click Here!
+        </button>
+        </div>
+    );
+}
---
+++ src/RequestFormButton.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+
+export default function RequestFormButton() {
+    const buttonStyling = React.useContext(StylesContext)
+
+    return (
+        <div>
+        <button>
+            Click Here!
+        </button>
+        <input type="submit" value="Submit" />
+
+        </div>
+    );
+}
